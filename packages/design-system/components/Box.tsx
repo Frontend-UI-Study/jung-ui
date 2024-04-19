fa5e8a7877f0b0bac451a4c9b1c232d681d63b2c
dgd03146 <ibory1220@naver.com>
2024-04-19 09:23:10 +0100

    feat: Implement Box component and atoms utility #4

---
+++ packages/design-system/components/Box.tsx
@@ -0,0 +1,25 @@
+import { type HTMLAttributes, forwardRef } from 'react';
+import { type Atoms, atoms, extractAtoms } from '../utils/atoms';
+
+type HTMLProperties = Omit<
+	React.AllHTMLAttributes<HTMLElement>,
+	'as' | 'className' | 'color' | 'height' | 'width'
+>;
+
+type BoxProps = HTMLAttributes<HTMLElement> &
+	HTMLProperties &
+	Atoms & {
+		as?: React.ElementType;
+	};
+
+export const Box = forwardRef<HTMLElement, BoxProps>((props, ref) => {
+	const { as: Component = 'div', ...other } = props;
+	const [atomsProps, propsToForward] = extractAtoms(other);
+	const className = atoms({
+		className: propsToForward?.className,
+		reset: typeof Component === 'string' ? Component : 'div',
+		...atomsProps,
+	});
+
+	return <Component {...propsToForward} className={className} ref={ref} />;
+});
---
+++ packages/design-system/utils/atoms.ts
@@ -0,0 +1,25 @@
+import { type Sprinkles, baseReset, elementResets, sprinkles } from '../styles';
+
+import clsx, { type ClassValue } from 'clsx';
+import { omit, pick } from 'lodash';
+export interface Atoms extends Sprinkles {
+	reset?: keyof JSX.IntrinsicElements;
+	className?: ClassValue;
+}
+
+const keys = Array.from(sprinkles.properties.keys());
+export const extractAtoms = <P extends object>(props: P) => [
+	pick(props, keys),
+	omit(props, keys),
+];
+
+export function atoms(atoms: Atoms) {
+	const { reset, className, ...rest } = atoms;
+	const sprinklesClassNames = sprinkles(rest);
+
+	return clsx(
+		sprinklesClassNames,
+		className,
+		reset ? [baseReset, elementResets[reset]] : null,
+	);
+}
